784ecd7b5407a23b8c42ff81636fd477aa998205
ELIZA <rysaeva96@mail.ru>
2022-06-10 20:17:28 +0500

    Задача 60

---
+++ Task_60/Program.cs
@@ -0,0 +1,49 @@
+﻿// Сформируйте трёхмерный массив из неповторяющихся
+// двузначных чисел. Напишите программу, которая 
+// будет построчно выводить массив, добавляя индексы 
+// каждого элемента.
+
+
+Console.WriteLine("Введите количество строк: ");
+int rows = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов: ");
+int cols = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов: ");
+int appliq = int.Parse(Console.ReadLine());
+
+int[,,] Create3DMatrix(int rows, int cols, int appliq)
+{
+    int[,,] matrix = new int[rows, cols, appliq];
+    Random rnd = new Random(Guid.NewGuid().GetHashCode());
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int z = 0; z < matrix.GetLength(2); z++)
+            {
+                matrix[i, j, z] = rnd.Next(10, 100);
+            
+            }
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int z = 0; z < matrix.GetLength(2); z++)
+            {
+                Console.Write(matrix[i, j, z] + " ");
+            }
+        }
+        Console.WriteLine();
+
+    }
+}
+
+int[,,] newmatrix = Create3DMatrix(rows, cols, appliq);
+PrintMatrix(newmatrix);
